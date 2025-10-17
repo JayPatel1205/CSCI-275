77df18217bb40a1f6ea69a3fdc5c8e8da0c3d79c
JayPatel1205 <jaypatel12005@gmail.com>
2025-10-16 23:56:55 -0700

    Revert "Sprint 2 "

--- backend/src/server.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-import app from "./app.js";
-
-dotenv.config();
-const port = process.env.PORT || 4000;
-
-app.listen(port, "0.0.0.0", () => {
-  console.log(`✅ Server running at http://localhost:${port}`);
-});
